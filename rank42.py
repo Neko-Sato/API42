21a2356adaae73b9b6d8cbe71eee42b7cb3ad247
Neko-Sato <hisatosi35350@gmail.com>
2024-09-20 03:58:23 +0900

    update

--- rank42.py
+++ rank42.py
@@ -4,41 +4,31 @@ import os
 
 async def get_level_rank(credential:API42.Credential) -> list:
 	query = {"cursus_id": 9, "filter[campus_id]": 26, "sort": "-level", "range[created_at]":"2024-08-20,now", "page[size]": 100}
-	users = []
-	tmp = (await credential.get("/v2/cursus_users", {**query, "page[number]": 1}))
-	users.extend(tmp)
-	tmp = (await credential.get("/v2/cursus_users", {**query, "page[number]": 2}))
-	users.extend(tmp)
-	result = []
-	for i, user in enumerate(users, 1):
-		t = {"rank": i, "user_id": user["user"]["id"] , "login": user["user"]["login"], "level": user["level"]}
-		result.append(t)
-	return result
+	users = [u for s in await asyncio.gather(
+		credential.get("/v2/cursus_users", {**query, "page[number]": 1}),
+		credential.get("/v2/cursus_users", {**query, "page[number]": 2})
+		) for u in s]
+	return [{"rank": i, "user_id": user["user"]["id"] , "login": user["user"]["login"], "level": user["level"]}
+		 for i, user in enumerate(users, 1)]
 
 async def get_score_rank(credential:API42.Credential) -> dict:
 	query = {"filter[coalition_id]": "61,62,63", "sort": "-this_year_score", "range[created_at]":"2024-08-20,now", "page[size]": 100}
-	users = []
-	tmp = (await credential.get("/v2/coalitions_users", {**query, "page[number]": 1}))
-	users.extend(tmp)
-	tmp = (await credential.get("/v2/coalitions_users", {**query, "page[number]": 2}))
-	users.extend(tmp)
-	login = []
+	users = [u for s in await asyncio.gather(
+		credential.get("/v2/coalitions_users", {**query, "page[number]": 1}),
+		credential.get("/v2/coalitions_users", {**query, "page[number]": 2})
+		) for u in s]
 	query = {"filter[id]": ",".join([str(user["user_id"]) for user in users]), "page[size]": 100}
-	tmp = (await credential.get("/v2/users", {**query, "page[number]": 1}))
-	login.extend(tmp)
-	tmp = (await credential.get("/v2/users", {**query, "page[number]": 2}))
-	login.extend(tmp)
-	login = {i["id"]:i["login"] for i in login}
-	result = []
-	for i, user in enumerate(users, 1):
-		t = {"rank": i, "login": login.get(user["user_id"], "Unknown"), "score": user["score"]}
-		result.append(t)
-	return result
+	login = {i["id"]:i["login"] for s in await asyncio.gather(
+		credential.get("/v2/users", {**query, "page[number]": 1}),
+		credential.get("/v2/users", {**query, "page[number]": 2})
+		) for i in s}
+	return [{"rank": i, "login": login.get(user["user_id"], "Unknown"), "score": user["score"]}
+		 for i, user in enumerate(users, 1)]
 
 async def get_rank(credential:API42.Credential) -> tuple[list, list]:
-	level_rank = await get_level_rank(credential)
-	score_rank = await get_score_rank(credential)
-	return level_rank, score_rank
+	# return await asyncio.gather(get_level_rank(credential), get_score_rank(credential))
+	# 効率的に取得しようと非同期にしたものの429エラーになりやがるからここは同期で取得する
+	return await get_level_rank(credential), await get_score_rank(credential)
 
 async def main():
 	client_id = os.getenv("API42_CLIENT_ID")
